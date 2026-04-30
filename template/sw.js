5f16d3b57efc7f4df712af53c013c6dd72543bb0
Arun mohan k <arunmohankrl@email.com>
2026-05-01 00:27:47 +0530

    Full PWA implementation with manifest, service worker, and meta tags

--- template/sw.js
+++ template/sw.js
@@ -1,26 +1,3 @@
-// Service Worker for SRM Match PWA - v2 (Cache cleared)
-const CACHE_NAME = 'srm-match-cache-v2';
-const urlsToCache = [];
-
-self.addEventListener('install', event => {
-  // Take control immediately - don't wait for old SW to die
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  // Let the browser handle all network requests natively.
-  // We keep this event listener active because Chrome requires a fetch handler 
-  // to recognize the app as an installable PWA and trigger the install banner.
-});
-
-self.addEventListener('activate', event => {
-  // Delete ALL old caches immediately
-  event.waitUntil(
-    caches.keys().then(cacheNames =>
-      Promise.all(cacheNames.map(name => caches.delete(name)))
-    ).then(() => self.clients.claim()) // Take control of all open pages
-  );
+self.addEventListener("fetch", function(event) {
+  event.respondWith(fetch(event.request));
 });
