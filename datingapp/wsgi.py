8e928b0b96cd7c6357a0c88c0dbde0a72413abe7
Arun mohan k <arunmohankrl@email.com>
2026-05-05 01:30:02 +0530

    chore: cleaned up debug logs and restored production entry points

--- datingapp/wsgi.py
+++ datingapp/wsgi.py
@@ -1,15 +1,7 @@
 import os
-import sys
-import traceback
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'datingapp.settings')
 
-try:
-    from django.core.wsgi import get_wsgi_application
-    application = get_wsgi_application()
-except Exception as e:
-    print("!!! WSGI STARTUP ERROR !!!")
-    traceback.print_exc()
-    raise e
-
+application = get_wsgi_application()
 app = application
--- index.py
+++ index.py
@@ -1,19 +1,9 @@
 import os
-import sys
-import traceback
+from django.core.wsgi import get_wsgi_application
 
 # Ensure settings module is set
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'datingapp.settings')
 
-print("--- STARTING IMPORT TEST ---")
-try:
-    import home.views
-    print("--- home.views IMPORT SUCCESS ---")
-except Exception as e:
-    print("!!! home.views IMPORT ERROR !!!")
-    print(f"Error: {type(e).__name__}: {str(e)}")
-    traceback.print_exc()
-
-from django.core.wsgi import get_wsgi_application
+# Standard Vercel entry point
 app = get_wsgi_application()
 application = app
